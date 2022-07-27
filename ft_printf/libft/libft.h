40a68cb8e02fa36b1c863419b6e2f7b96d3dbbdb
Vardanyan-Hovo <hovhannes_vardanyan1@mail.ru>
2022-07-27 10:42:11 +0400

    printf

---
+++ ft_printf/libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvardany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 16:50:05 by hvardany          #+#    #+#             */
+/*   Updated: 2022/04/07 20:53:50 by hvardany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+
+#endif
